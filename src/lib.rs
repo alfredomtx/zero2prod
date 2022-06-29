e41925ba4c873ed6fba23408549e1a08685841a5
Alfredo Menezes <amtrosul@hotmail.com>
2022-06-29 20:14:07 -0300

    organized files, added postgre dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,5 @@
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
-use actix_web::dev::Server;
-use std::net::TcpListener;
 
-#[get("/health_check")]
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok().finish()
-}
+pub mod configuration;
+pub mod routes;
+pub mod startup;
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
-            App::new().service(health_check)
-        })
-        .listen(listener)?
-        .run();
-
-    Ok(server)
-}
