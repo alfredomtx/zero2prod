b4a22f8e5509813bf3a8fca86fb87f255688587d
Alfredo Menezes <amtrosul@hotmail.com>
2022-06-29 15:51:23 -0300

    finished health_check implementantion and tests
    also added random port use for the app

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,11 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
-
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
-}
+use zero2prod::run;
+use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-    App::new()
-    .route("/", web::get().to(greet))
-    .route("/{name}", web::get().to(greet))
-    })
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await
-
-
+    // Bubble up the io::Error if we failed to bind the address
+    // Otherwise call .await on our Server
+    let listener = TcpListener::bind("127.0.0.1:0")
+        .expect("Failed to bind random port");
+    run(listener)?.await
 }
