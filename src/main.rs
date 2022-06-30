fd3d05807f2eb6c172edcb630855fb4d13ce18d7
Alfredo Menezes <amtrosul@hotmail.com>
2022-06-29 23:34:08 -0300

    implementing connection pool for postgres

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
 use std::net::TcpListener;
 use zero2prod::startup::run;
 use zero2prod::configuration::get_configuration;
+use sqlx::{PgPool};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     // Panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
+
     let address = format!("127.0.0.1:{}", configuration.application_port);
     // Bubble up the io::Error if we failed to bind the address
     // Otherwise call .await on our Server
     let listener = TcpListener::bind(address)
         .expect("Failed to bind random port");
-    run(listener)?.await
+    run(listener, connection_pool)?.await
 }
