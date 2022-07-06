fabf2b1f3b039349eb9a43dd82c7ba5a919d7639
Alfredo Menezes <amtrosul@hotmail.com>
2022-07-05 22:42:34 -0300

    dockerfile configured and ready to deploy on digitalocean

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
-use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
 use secrecy::ExposeSecret;
 
@@ -15,11 +15,14 @@ async fn main() -> std::io::Result<()> {
 
     // Panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPool::connect(&configuration.database.connection_string().expose_secret())
-        .await
+    let connection_pool = PgPoolOptions::new()
+        .acquire_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy(&configuration.database.connection_string().expose_secret())
         .expect("Failed to connect to Postgres.");
 
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let address = format!("{}:{}"
+        , configuration.application.host, configuration.application.port
+    );
     // Bubble up the io::Error if we failed to bind the address
     // Otherwise call .await on our Server
     let listener = TcpListener::bind(address).expect("Failed to bind random port");
