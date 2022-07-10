687d4ede136253b3b4a3de23fdef18fb20b59b35
Alfredo Menezes <amtrosul@hotmail.com>
2022-07-10 16:25:44 -0300

    fixed get_configuration

--- src/configuration.rs
+++ src/configuration.rs
@@ -57,13 +57,8 @@ impl DatabaseSettings {
 }
 
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
-    // Initialise our configuration reader
-    let mut settings = config::Config::default();
-    let base_path = std::env::current_dir().expect("Failed to get current directory.");
+    let base_path = std::env::current_dir().expect("Failed to determine the current directory");
     let configuration_directory = base_path.join("configuration");
-
-    // Read the "default" configuration file
-    settings.merge(config::File::from(configuration_directory.join("base")).required(true))?;
     
     // Detect the running environment
     // Default to local if unspecified
@@ -71,18 +66,17 @@ pub fn get_configuration() -> Result<Settings, config::ConfigError> {
         .unwrap_or_else(|_| "local".into())
         .try_into()
         .expect("Failed to parse APP_ENVIRONMENT.");
-
-    // Layer on the environment-specific values.
-    settings.merge(
-        config::File::from(configuration_directory.join(environment.as_str())).required(true)
-    )?;
-    // Add in settings from environment variables (with a prefix of APP and '__' as separator)
-    // E.g. `APP_APPLICATION__PORT=5001 would set `Settings.application.port`
-    settings.merge(config::Environment::with_prefix("app").separator("__"))?;
-
-    // Try to convert the configuration values it read into
-    // our Settings type
-    settings.try_deserialize()
+    
+    let environment_filename = format!("{}.yaml", environment.as_str());
+    let settings = config::Config::builder()
+        .add_source(config::File::from(configuration_directory.join("base.yaml")))
+        .add_source(config::File::from(configuration_directory.join(&environment_filename)))
+        // Add in settings from environment variables (with a prefix of APP and '__' as separator)
+        // E.g. `APP_APPLICATION__PORT=5001 would set `Settings.application.port`
+        .add_source(config::Environment::with_prefix("APP").prefix_separator("_").separator("__"))
+        .build()?;
+    
+    settings.try_deserialize::<Settings>()
 }
 
 /// The possible runtime environment for our application.
--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,6 @@ use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
 
 
-
-
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
