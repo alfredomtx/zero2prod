67de14645600d84152bf0c26524fb95612f66928
Alfredo Menezes <amtrosul@hotmail.com>
2022-10-02 21:32:47 -0400

    test refactor

--- tests/api/subscriptions.rs
+++ tests/api/subscriptions.rs
@@ -20,9 +20,24 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
 
     // Assert
     assert_eq!(200, response.status().as_u16());
+}
+
+#[tokio::test]
+async fn subscribe_persists_the_new_subscriber() {
+    // Arrange
+    let app = spawn_app().await;
+    let body = "name=Alfredo&email=ursula_le_guin%40gmail.com";
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .mount(&app.email_server)
+        .await;
 
     // Act
-    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
+    app.post_subscriptions(body.into()).await;
+
+    let saved = sqlx::query!("SELECT email, name, status FROM subscriptions",)
         .fetch_one(&app.db_pool)
         .await
         .expect("Failed to fecth saved subscription.");
@@ -30,6 +45,7 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
     // Assert
     assert_eq!(saved.name, "Alfredo");
     assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.status, "pending_confirmation");
 }
 
 #[tokio::test]
