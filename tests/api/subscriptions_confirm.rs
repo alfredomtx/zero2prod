c2b7d949aec2c1c7e18348553e3aa9fa9a20ee7c
Alfredo Menezes <amtrosul@hotmail.com>
2022-10-03 23:35:46 -0400

    page 333 - still need to fix failing tests

--- tests/api/subscriptions_confirm.rs
+++ tests/api/subscriptions_confirm.rs
@@ -18,7 +18,7 @@ async fn confirmations_without_token_are_rejected_with_a_400() {
 }
 
 #[tokio::test]
-async fn the_link_returned_by_subscribe_returns_a_200_if_called(){
+async fn the_link_returned_by_subscribe_returns_a_200_if_called() {
     // Arrange
     let app = spawn_app().await;
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
@@ -31,12 +31,10 @@ async fn the_link_returned_by_subscribe_returns_a_200_if_called(){
 
     app.post_subscriptions(body.into()).await;
     let email_request = &app.email_server.received_requests().await.unwrap()[0];
-    let confirmation_links = app.get_confirmation_links(&email_request);
+    let confirmation_links = app.get_confirmation_links(email_request);
 
     // Act
-    let response = reqwest::get(confirmation_links.html)
-        .await
-        .unwrap();
+    let response = reqwest::get(confirmation_links.html).await.unwrap();
 
     // Assert
     assert_eq!(response.status().as_u16(), 200);
